fd5282c1973b3a99401ae381d032e31a083f864e
Турсунов Баходур <tursunovb18@gmail.com>
2024-09-26 16:33:56 +0500

    Added some programs

---
+++ day1/variablesAndConsts.ts
@@ -0,0 +1,28 @@
+// let x = 10;
+// {
+//   let x = 25;
+//   console.log(x);
+// }
+// console.log(x);
+/////////////////////////////////////////////
+// let q = 10;
+// {
+//   let q = 25;
+//   {
+//     let q = 123;
+//     console.log(q);
+//   }
+//   console.log(q);
+// }
+
+// console.log(q);
+
+{
+  var x = 24;
+}
+console.log(x);
+
+{
+  let t = 10; // область видимости.
+}
+console.log(t);
